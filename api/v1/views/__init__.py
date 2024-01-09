aa29c0f3374b3509d0b905a6d0da8ede1aedabaa
astianmuchui <astianmuchui@gmail.com>
2024-01-09 13:11:28 +0300

    Status task

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,10 +3,3 @@
 from flask import Blueprint, jsonify
 from api.v1.views.index import *
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-@app_views.route('/status', strict_slashes=False)
-def status():
-    """
-    returns a JSON string: "status": "OK"
-    """
-    return jsonify({"status": "OK"})
