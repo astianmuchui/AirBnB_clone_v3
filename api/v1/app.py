40af8bde9356e3adcdd82622cfef9f3a92bae3af
astianmuchui <astianmuchui@gmail.com>
2024-01-09 13:47:09 +0300

    fix bugs

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,11 +9,10 @@ and registers blueprints from views
 Additionally, it handles teardown
 of the SQLAlchemy ORM and runs the app
 """
-
+from os import getenv
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
-from sys import environ
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -24,7 +23,8 @@ def teardown_db(self):
     storage.close()
 
 
-if __name__ == "__main__":
-    api_host = "0.0.0.0" if environ.get('HBNB_API_HOST') is None else "0.0.0.0"
-    api_port = "5000" if environ.get('HBNB_API_PORT') is None else "5000"
+if __name__ == '__main__':
+    api_host = getenv('HBNB_API_HOST', '0.0.0.0')
+    api_port = getenv('HBNB_API_PORT', 5000)
+
     app.run(host=api_host, port=api_port, threaded=True)
